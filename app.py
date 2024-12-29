984bf2df2d71472341e5c9f5231f19f369589a26
Nicolas Garcia <nicolasgarcia.7b@gmail.com>
2024-12-29 14:11:09 -0500

    Adición de código y archivos de configuración adicionales

--- app.py
+++ app.py
@@ -1,3 +1,30 @@
 import pandas as pd
 import streamlit as stl
 import plotly.express as px
+
+df = pd.read_csv(vehicles_us.csv)
+
+hist_button = stl.button("Crear Histograma", type="primary") # Crear botón que genera el histograma
+
+if hist_button:
+    # Escribir el mensaje de salida
+    stl.header("Histograma creado")
+    
+    # Crear el gráfico
+    fig = px.histogram(df, x="odometer")
+    
+    # Mostrar gráfico interactivo en Streamlit
+    stl.plotly_chart(fig, use_container_width=True)
+
+
+scatter_button = stl.button("Crear Gráfico de Dispersión", type="primary") # Crear botón que genera el gráfico de dispersión
+
+if hist_button:
+    # Escribir el mensaje de salida
+    stl.header("Gráfico de Dipsersión creado")
+    
+    # Crear el gráfico
+    fig = px.histogram(df, x="odometer", y="condition")
+    
+    # Mostrar gráfico interactivo en Streamlit
+    stl.plotly_chart(fig, use_container_width=True)
